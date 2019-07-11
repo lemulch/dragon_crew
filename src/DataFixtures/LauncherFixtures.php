c33596eba44ffefb4634edcd745e57d818ceaaed
lemulch <leochevallier601@gmail.com>
2019-07-11 16:17:30 +0200

    photoIvan

--- src/DataFixtures/LauncherFixtures.php
+++ src/DataFixtures/LauncherFixtures.php
@@ -14,6 +14,7 @@ class LauncherFixtures extends Fixture
         $launcher1->setName("Falcon 9");
         $launcher1->setType("Machin truc");
         $launcher1->setConstructor("Space X");
+        $launcher1->setPhoto("Vol_1.png");
         $this->setReference("launcher-falcon9", $launcher1);
         $manager->persist($launcher1);
 
@@ -21,6 +22,7 @@ class LauncherFixtures extends Fixture
         $launcher2->setName("Longue marche");
         $launcher2->setType("Machin");
         $launcher2->setConstructor("Les noichi");
+        $launcher2->setPhoto("Vol_2.png");
         $this->setReference("launcher-longuemarche", $launcher2);
         $manager->persist($launcher2);
 
@@ -28,6 +30,7 @@ class LauncherFixtures extends Fixture
         $launcher3->setName("Falcon Heavy");
         $launcher3->setType("bigfuckingrocket");
         $launcher3->setConstructor("Space X");
+        $launcher3->setPhoto("Vol_3.png");
         $this->setReference("launcher-falconheavy", $launcher3);
         $manager->persist($launcher3);
 
@@ -35,6 +38,7 @@ class LauncherFixtures extends Fixture
         $launcher4->setName("Delta IV");
         $launcher4->setType("Machin truc");
         $launcher4->setConstructor("NASA");
+        $launcher4->setPhoto("Vol_1.png");
         $this->setReference("launcher-delta4", $launcher4);
         $manager->persist($launcher4);
 
@@ -42,6 +46,7 @@ class LauncherFixtures extends Fixture
         $launcher5->setName("Ariane 5");
         $launcher5->setType("Machin truc");
         $launcher5->setConstructor("arianespace");
+        $launcher5->setPhoto("Vol_2.png");
         $this->setReference("launcher-ariane5", $launcher5);
         $manager->persist($launcher5);
 
@@ -49,6 +54,7 @@ class LauncherFixtures extends Fixture
         $launcher6->setName("Soyouz");
         $launcher6->setType("Machin truc");
         $launcher6->setConstructor("cyka blyat");
+        $launcher6->setPhoto("Vol_3.png");
         $this->setReference("launcher-soyouz", $launcher6);
         $manager->persist($launcher6);
 
